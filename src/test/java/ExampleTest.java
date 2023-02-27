7ca5b1c9638b76c88bd5744e202bdaf1ad96e40e
Tamara <color.ui>
2023-02-27 18:54:17 +0300

    new

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -1,2 +1,152 @@
-package PACKAGE_NAME;public class ExampleTest {
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class ExampleTest extends AbstractTest {
+
+    @Test
+    void getConnectUsers(){
+        given()
+                .queryParams("username", "Tamara")
+                .queryParam("hash", "24170312")
+                .pathParam("id", 716429)
+                .when()
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
+    void getRequestDataTest() {
+        given()
+                .queryParam("apiKey", getApiKey())
+                .queryParam("includeNutrition", "false")
+                .pathParam("id", 716429)
+                .when()
+                .get(getBaseUrl()+"recipes/{id}/information")
+                .then()
+                .statusCode(200);
+
+        given()
+                .queryParam("apiKey", getApiKey())
+                .queryParam("includeNutrition", "false")
+                .pathParam("id", 654959)
+                .when()
+                .get(getBaseUrl()+"recipes/{id}/information")
+                .then()
+                .statusCode(200);
+
+        given()
+                .queryParam("apiKey", getApiKey())
+                .queryParam("includeNutrition", "false")
+                .pathParam("id", 664147)
+                .when()
+                .get(getBaseUrl()+"recipes/{id}/information")
+                .then()
+                .statusCode(200);
+
+        given()
+                .queryParam("apiKey", getApiKey())
+                .queryParam("includeNutrition", "false")
+                .pathParam("id", 716426)
+                .when()
+                .get(getBaseUrl()+"recipes/{id}/information")
+                .then()
+                .statusCode(200);
+
+        given()
+                .queryParam("apiKey", getApiKey())
+                .queryParam("includeNutrition", "false")
+                .pathParam("id", 782585)
+                .when()
+                .get(getBaseUrl()+"recipes/{id}/information")
+                .then()
+                .statusCode(200);
+
+
+
+        given()
+                .queryParam("apiKey", getApiKey())
+                .contentType("application/x-www-form-urlencoded")
+                .formParam("title","Pork roast with green beans")
+                .when()
+                .post(getBaseUrl()+"recipes/cuisine")
+                .then()
+                .statusCode(200);
+
+        given()
+                .queryParam("apiKey", getApiKey())
+                .contentType("application/x-www-form-urlencoded")
+                .formParam("title","Pasta With Tuna")
+                .when()
+                .post(getBaseUrl()+"recipes/cuisine")
+                .then()
+                .statusCode(200);
+
+        given()
+                .queryParam("apiKey", getApiKey())
+                .contentType("application/x-www-form-urlencoded")
+                .formParam("title","Tuscan White Bean Soup with Olive Oil and Rosemary")
+                .when()
+                .post(getBaseUrl()+"recipes/cuisine")
+                .then()
+                .statusCode(200);
+
+        given()
+                .queryParam("apiKey", getApiKey())
+                .contentType("application/x-www-form-urlencoded")
+                .formParam("title","Cannellini Bean and Asparagus Salad with Mushrooms")
+                .when()
+                .post(getBaseUrl()+"recipes/cuisine")
+                .then()
+                .statusCode(200);
+
+        given()
+                .queryParam("apiKey", getApiKey())
+                .contentType("application/x-www-form-urlencoded")
+                .formParam("title","Cauliflower, Brown Rice, and Vegetable Fried Rice")
+                .when()
+                .post(getBaseUrl()+"recipes/cuisine")
+                .then()
+                .statusCode(200);
 }
+
+
+    String id;
+    @Test
+    void addMealTest() {
+        id = given()
+                .queryParam("hash", "a3da66460bfb7e62ea1c96cfa0b7a634a346ccbf")
+                .queryParam("apiKey", "605332b375604bea9b1446deaac89526")
+                .body("{\n"
+                        + " \"date\": 1644881179,\n"
+                        + " \"slot\": 1,\n"
+                        + " \"position\": 0,\n"
+                        + " \"type\": \"INGREDIENTS\",\n"
+                        + " \"value\": {\n"
+                        + " \"ingredients\": [\n"
+                        + " {\n"
+                        + " \"name\": \"1 banana\"\n"
+                        + " }\n"
+                        + " ]\n"
+                        + " }\n"
+                        + "}")
+                .when()
+                .post("https://api.spoonacular.com/mealplanner/geekbrains/items")
+                .then()
+                .statusCode(200)
+                .extract()
+                .jsonPath()
+                .get("id")
+                .toString();
+    }
+    @BeforeAll
+    static void setUp(){
+
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+    }
+
+}
+
